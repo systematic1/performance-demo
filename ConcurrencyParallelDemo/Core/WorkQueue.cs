6e6b652096d3998626e7d96a76e8c87302b785d3
kelly <systematic1@yahoo.com>
2026-09-18 21:30:13 -0400

    Created Concurrency Parallel Demo project

---
+++ ConcurrencyParallelDemo/Core/WorkQueue.cs
@@ -0,0 +1,67 @@
+using System.Buffers;
+
+namespace ConcurrencyParallelDemo.Core;
+
+// This is a basic implementation of a lock-free ring queue based on a fixed
+//  rented ArrayPool. It can store up to 16,384 items at any one time.
+public class WorkQueue : IDisposable
+{
+    private static readonly int Capacity = 16 * 1024;
+    
+    private readonly ProductOrder[] _orders;
+    private long _head;
+    private long _tail;
+    
+    public WorkQueue()
+    {
+        _orders = ArrayPool<ProductOrder>.Shared.Rent(Capacity);
+        _head = 0; // dequeue from
+        _tail = 0; // enqueue at
+    }
+
+    public bool IsEmpty()
+    {
+        return _head % Capacity == _tail % Capacity;
+    }
+
+    public bool IsFull()
+    {
+        return (_tail - _head) >= Capacity;
+    }
+
+    public long Count()
+    {
+        return (_tail - _head) % Capacity;
+    }
+
+    public bool Enqueue(ref ProductOrder order)
+    {
+        if (!IsFull())
+        {
+            _orders[_tail % Capacity] = order;
+            _tail++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool Dequeue(ref ProductOrder? order)
+    {
+        if (!IsEmpty())
+        {
+            order = _orders[_head % Capacity];
+            _head++;
+            return true;
+        }
+
+        order = default(ProductOrder);
+        return false;
+    }
+    
+    public void Dispose()
+    {
+        if (_orders?.Length > 0)
+            ArrayPool<ProductOrder>.Shared.Return(_orders);
+    }
+}
---
+++ ConcurrencyParallelDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Buffers;
+
+namespace ConcurrencyParallelDemo;
+
+public static class Program
+{
+    // static members
+    private static readonly int Capacity = 512 * 1024;
+    
+    static void Main(string[] args)
+    {
+        // Multithreaded simultaneous operations/calculations
+        // Parse and transform generated (incoming) data and send to multiple
+        //  processing engines (threads) to perform different operations on each
+        // Minimize/eliminate memory allocation
+        // Use Parallel.For/Each() to perform parallel CPU tasking
+    }
+
+    public static void PreAllocateWorkQueueArray()
+    {
+        // This allocates a fixed size array up front to rent to both the OrderQueue and the generator.
+        // The generator and consumer classes will rent from this ArrayPool and must return them.
+        var tempArray = ArrayPool<ProductOrder>.Shared.Rent(Capacity);
+        ArrayPool<ProductOrder>.Shared.Return(tempArray);
+    }
+}
